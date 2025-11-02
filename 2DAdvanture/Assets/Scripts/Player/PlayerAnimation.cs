e397753acf14f0e76cdc99f1f91983c42edd616b
brfwind <19172405320@163.com>
2025-11-02 14:51:07 +0800

    feat: 下蹲逻辑(含修改bug)

--- 2DAdvanture/Assets/Scripts/Player/PlayerAnimation.cs
+++ 2DAdvanture/Assets/Scripts/Player/PlayerAnimation.cs
@@ -7,6 +7,7 @@ public class PlayerAnimation : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
     private PhysicsCheck pc;
+    private PlayerController playerController;
 
     //获取组件
     private void Awake()
@@ -14,6 +15,7 @@ public class PlayerAnimation : MonoBehaviour
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         pc = GetComponent<PhysicsCheck>();
+        playerController = GetComponent<PlayerController>();
     }
 
     //实时检测更新动画转变参数
@@ -28,5 +30,6 @@ public class PlayerAnimation : MonoBehaviour
         anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
         anim.SetFloat("velocityY", rb.velocity.y);
         anim.SetBool("isGround", pc.isGround);
+        anim.SetBool("isCrouch", playerController.isCrouch);
     }
 }
--- 2DAdvanture/Assets/Scripts/Player/PlayerContraller.cs
+++ 2DAdvanture/Assets/Scripts/Player/PlayerContraller.cs
@@ -9,17 +9,25 @@ public class PlayerController : MonoBehaviour
     public PlayerInputControl inputControl;
     public Rigidbody2D rb;
     public PhysicsCheck pc;
+    public CapsuleCollider2D cc;
     public Vector2 inputDirection;
     [Header("基本参数")]
     public float speed;
     private float runSpeed;
     private float walkSpeed => speed / 2.7f; 
     public float jumpForce;
+    public bool isCrouch;
+    private Vector2 originalOffset;
+    private Vector2 originalSize;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         pc = GetComponent<PhysicsCheck>();
+        cc = GetComponent<CapsuleCollider2D>();
+
+        originalOffset = cc.offset;
+        originalSize = cc.size;
 
         inputControl = new PlayerInputControl();
 
@@ -66,7 +74,11 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         //Player的刚体组件中的速度，等于算出的速度（speed * 输入的x值 ， rb自己的y速度）
-        rb.velocity = new Vector2(speed * Time.deltaTime * inputDirection.x, rb.velocity.y);
+        //如果正在下蹲，让rb.velocity等于0，人物立刻停止移动，防止方向键和下蹲键同时按下时，人物蹲不下来
+        if (isCrouch)
+            rb.velocity = new Vector2(0f, 0f);
+        else
+            rb.velocity = new Vector2(speed * Time.deltaTime * inputDirection.x, rb.velocity.y);
 
         int faceDir = (int)transform.localScale.x;
 
@@ -77,6 +89,19 @@ public class PlayerController : MonoBehaviour
 
         //根据运动方向，改变Player面部朝向
         transform.localScale = new Vector3(faceDir, 1, 1);
+
+        if (inputDirection.y < -0.5f && pc.isGround)
+        {
+            isCrouch = true;
+            cc.offset = new Vector2(-0.11f, 0.8f);
+            cc.size = new Vector2(0.7f, 1.6f);
+        }
+        else
+        {
+            isCrouch = false;
+            cc.offset = originalOffset;
+            cc.size = originalSize;
+        }
     }
 
     //跳跃方法（通过施加力）
